1da0c19328c25a8e46e72672ec6c8154dc680108
douglas man <douglas.man.packt@outlook.com>
2020-07-22 21:29:07 -0400

    contacts component ...

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { ContactsComponent } from './contacts/contacts.component';
+
+const routes: Routes = [
+  { path: '', component: ContactsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // import {MatToolbarModule} from '@angular/material/toolbar';
 import { AppMaterialModule } from './app.material.module';
+import { ContactsComponent } from './contacts/contacts.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
